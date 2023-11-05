304d8703fb2df153f633d0e27d38bd19fdda7393
glenwojnar1.0@gmail.com <glenwojnar1.0@gmail.com>
2023-11-05 15:08:05 -0500

    got player model working

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,74 +1,68 @@
-#include "Platform/Platform.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
 
-int main()
-{
-	util::Platform platform;
+class Player {
+public:
+    Player() {
+        // Initialize player's position, speed, and other attributes
+        // You can add more properties as needed
+        playerShape = sf::ConvexShape(3); // Create a triangle shape
+        playerShape.setPoint(0, sf::Vector2f(0, -20));
+        playerShape.setPoint(1, sf::Vector2f(-20, 20));
+        playerShape.setPoint(2, sf::Vector2f(20, 20));
+        playerShape.setFillColor(sf::Color::White);
+        playerShape.setPosition(400, 300); // Set the initial position
+    }
 
-#if defined(_DEBUG)
-	std::cout << "Hello World!" << std::endl;
-#endif
+    void update([[maybe_unused]] sf::RenderWindow& window) {
+        // Implement player's movement and shooting logic here
+        // You can use keyboard or mouse input for control
 
-	sf::RenderWindow window;
+        // For example, move the player left when the A key is pressed
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            playerShape.move(-5.0f, 0);
+        }
+        // Add similar logic for other controls
+    }
 
-	// If needed
-	// float screenScale = platform.getScreenScalingFactor(window.getSystemHandle());
+    void draw([[maybe_unused]] sf::RenderWindow& window) {
+        // Render the player's shape on the window
+        window.draw(playerShape);
+    }
 
-	sf::Vector2u resolution{ 640, 480 };
-	sf::VideoMode mode(resolution.x, resolution.y);
+private:
+    sf::ConvexShape playerShape; // Shape representing the player
+};
 
-	sf::ContextSettings settings;
-#if !defined(SFML_SYSTEM_MACOS)
-// settings.majorVersion = 4;
-// settings.minorVersion = 1;
-// settings.depthBits = 24;
-// settings.stencilBits = 8;
-// settings.attributeFlags = sf::ContextSettings::Attribute::Core;
-#endif
-	window.create(mode, "SFML works!", sf::Style::Default, settings);
-	platform.initialize(window.getSystemHandle());
+int main() {
+    sf::RenderWindow window;
+    window.create(sf::VideoMode(800, 600), "Asteroids", sf::Style::Default);
+    window.setFramerateLimit(60);
+    window.clear(sf::Color::Black); // Set the background color to black
 
-	[](const sf::ContextSettings& inSettings) {
-		std::cout << "OpenGL context created with version: "
-				  << inSettings.majorVersion << "." << inSettings.minorVersion
-				  << " with " << inSettings.depthBits << " depth bits, "
-				  << inSettings.stencilBits << " stencil bits, "
-				  << (inSettings.attributeFlags == sf::ContextSettings::Attribute::Core ? "Core Profile" : "Compatibility Profile")
-				  << std::endl;
-	}(window.getSettings());
+    Player player; // Create the player object
 
-	auto shapeTexture = std::make_unique<sf::Texture>();
-	if (!shapeTexture->loadFromFile("content/sfml.png"))
-		return EXIT_FAILURE;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
 
-	sf::CircleShape shape(static_cast<float>(shapeTexture->getSize().y) / 2.0f);
-	shape.setFillColor(sf::Color::White);
-	shape.setPosition(
-		static_cast<float>(mode.width / 2) - shape.getRadius(),
-		static_cast<float>(mode.height / 2) - shape.getRadius()
-	);
+        // Handle player controls
+        player.update(window);
 
-	shape.setTexture(shapeTexture.get());
+        window.clear(); // Clear the window (clears to the background color)
 
-	sf::Event event;
+        // Draw game objects, including the player's shape
+        player.draw(window);
 
-	sf::Color clearColor{ 100, 149, 237 };
+        window.display();
+    }
 
-	window.setFramerateLimit(30);
-	window.clear(clearColor);
-	window.display();
-
-	while (window.isOpen())
-	{
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear(clearColor);
-		window.draw(shape);
-		window.display();
-	}
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
+
+
